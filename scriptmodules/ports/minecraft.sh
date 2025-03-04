7e8a057e28be3763798105ddb03e5907ecb3b493
LizDerby140 <64528822+LizDerby140@users.noreply.github.com>
2025-03-03 19:37:09 -0700

    Update minecraft.sh

--- scriptmodules/ports/minecraft.sh
+++ scriptmodules/ports/minecraft.sh
@@ -13,7 +13,7 @@ rp_module_id="minecraft"
 rp_module_desc="Minecraft - Pi Edition"
 rp_module_licence="PROP"
 rp_module_section="exp"
-rp_module_flags="!all videocore"
+rp_module_flags="!all videocore lepotato"
 
 function depends_minecraft() {
     getDepends xorg matchbox-window-manager
