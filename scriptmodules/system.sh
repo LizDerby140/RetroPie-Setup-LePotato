df4dcef8f495e917ec330c8d95ccbb464433bbc0
LizDerby140 <64528822+LizDerby140@users.noreply.github.com>
2025-03-04 05:44:44 -0700

    Update system.sh

--- scriptmodules/system.sh
+++ scriptmodules/system.sh
@@ -707,5 +707,5 @@ function platform_vero4k() {
 
 function platform_lepotato() {
     cpu_armv8 "cortex-a53"
-    __platform_flags+=(arm mali neon x11 gles2)
+    __platform_flags+=(arm mali neon x11 gles2 drm kms)
 }
