2eef564a94de998973f1b6865d3ea22805a0545e
LizDerby140 <64528822+LizDerby140@users.noreply.github.com>
2025-03-03 23:46:41 -0700

    Update lr-dosbox.sh

--- scriptmodules/libretrocores/lr-dosbox.sh
+++ scriptmodules/libretrocores/lr-dosbox.sh
@@ -15,7 +15,7 @@ rp_module_help="ROM Extensions: .bat .com .exe .sh\n\nCopy your DOS games to $RO
 rp_module_licence="GPL2 https://raw.githubusercontent.com/libretro/dosbox-libretro/master/COPYING"
 rp_module_repo="git https://github.com/libretro/dosbox-libretro.git master"
 rp_module_section="exp"
-rp_module_flags=""
+rp_module_flags="lepotato 64bit aarch64 arm mali"
 
 function sources_lr-dosbox() {
     gitPullOrClone
