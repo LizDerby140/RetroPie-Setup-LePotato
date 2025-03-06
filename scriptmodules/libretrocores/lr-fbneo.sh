2c696e8121a9b07e96bb7db7c06b9cdf7d10b727
LizDerby140 <64528822+LizDerby140@users.noreply.github.com>
2025-03-06 03:09:44 -0700

    Update lr-fbneo.sh

--- scriptmodules/libretrocores/lr-fbneo.sh
+++ scriptmodules/libretrocores/lr-fbneo.sh
@@ -10,11 +10,10 @@
 #
 
 rp_module_id="lr-fbneo"
-rp_module_desc="Arcade emu - FinalBurn Neo (latest version) port for libretro"
+rp_module_desc="Arcade emu - FinalBurn Neo v0.2.97.44 (WIP) port for libretro"
 rp_module_help="Previously called lr-fba-next and fbalpha\n\ROM Extension: .zip\n\nCopy your FBA roms to\n$romdir/fba or\n$romdir/neogeo or\n$romdir/arcade\n\nFor NeoGeo games the neogeo.zip BIOS is required and must be placed in the same directory as your FBA roms."
 rp_module_licence="NONCOM https://raw.githubusercontent.com/libretro/FBNeo/master/src/license.txt"
-rp_module_repo="git https://github.com/libretro/FBNeo.git master"
-rp_module_section="opt"
+rp_module_section="main"
 
 function _update_hook_lr-fbneo() {
     # move from old location and update emulators.cfg
@@ -23,7 +22,7 @@ function _update_hook_lr-fbneo() {
 }
 
 function sources_lr-fbneo() {
-    gitPullOrClone
+    gitPullOrClone "$md_build" https://github.com/libretro/FBNeo.git
 }
 
 function build_lr-fbneo() {
@@ -39,16 +38,34 @@ function build_lr-fbneo() {
 
 function install_lr-fbneo() {
     md_ret_files=(
-        'fbahelpfilesrc/fbneo.chm'
+        'fba.chm'
         'src/burner/libretro/fbneo_libretro.so'
         'gamelist.txt'
         'whatsnew.html'
+        'preset-example.zip'
         'metadata'
         'dats'
     )
 }
 
 function configure_lr-fbneo() {
+    local dir
+    for dir in arcade fba neogeo; do
+        mkRomDir "$dir"
+        ensureSystemretroconfig "$dir"
+    done
+
+    # Create samples directory
+    mkUserDir "$biosdir/fbneo"
+    mkUserDir "$biosdir/fbneo/samples"
+
+    # copy hiscore.dat
+    cp "$md_inst/metadata/hiscore.dat" "$biosdir/fbneo/"
+    chown $user:$user "$biosdir/fbneo/hiscore.dat"
+
+    # Set core options
+    setRetroArchCoreOption "fbneo-diagnostic-input" "Hold Start"
+
     local def=1
     isPlatform "armv6" && def=0
     addEmulator 0 "$md_id" "arcade" "$md_inst/fbneo_libretro.so"
@@ -68,54 +85,17 @@ function configure_lr-fbneo() {
     addEmulator 0 "$md_id-cv" "coleco" "$md_inst/fbneo_libretro.so --subsystem cv"
     addEmulator 0 "$md_id-msx" "msx" "$md_inst/fbneo_libretro.so --subsystem msx"
     addEmulator 0 "$md_id-spec" "zxspectrum" "$md_inst/fbneo_libretro.so --subsystem spec"
-    addEmulator 0 "$md_id-fds" "fds" "$md_inst/fbneo_libretro.so --subsystem fds"
-    addEmulator 0 "$md_id-nes" "nes" "$md_inst/fbneo_libretro.so --subsystem nes"
-    addEmulator 0 "$md_id-ngp" "ngp" "$md_inst/fbneo_libretro.so --subsystem ngp"
-    addEmulator 0 "$md_id-ngpc" "ngpc" "$md_inst/fbneo_libretro.so --subsystem ngp"
-    addEmulator 0 "$md_id-chf" "channelf" "$md_inst/fbneo_libretro.so --subsystem chf"
 
-    local systems=(
-        "arcade"
-        "neogeo"
-        "fba"
-        "pcengine"
-        "gamegear"
-        "mastersystem"
-        "megadrive"
-        "sg-1000"
-        "coleco"
-        "msx"
-        "zxspectrum"
-        "fds"
-        "nes"
-        "ngp"
-        "ngpc"
-        "channelf"
-    )
-
-    local system
-    for system in "${systems[@]}"; do
-        addSystem "$system"
-    done
-
-    [[ "$md_mode" == "remove" ]] && return
-
-    for system in "${systems[@]}"; do
-        mkRomDir "$system"
-        defaultRAConfig "$system"
-    done
-
-    # Create directories for all support files
-    mkUserDir "$biosdir/fbneo"
-    mkUserDir "$biosdir/fbneo/blend"
-    mkUserDir "$biosdir/fbneo/cheats"
-    mkUserDir "$biosdir/fbneo/patched"
-    mkUserDir "$biosdir/fbneo/samples"
-
-    # copy hiscore.dat
-    cp "$md_inst/metadata/hiscore.dat" "$biosdir/fbneo/"
-    chown "$__user":"$__group" "$biosdir/fbneo/hiscore.dat"
-
-    # Set core options
-    setRetroArchCoreOption "fbneo-diagnostic-input" "Hold Start"
+    addSystem "arcade"
+    addSystem "neogeo"
+    addSystem "fba"
+    
+    addSystem "pcengine"
+    addSystem "gamegear"
+    addSystem "mastersystem"
+    addSystem "megadrive"
+    addSystem "sg-1000"
+    addSystem "coleco"
+    addSystem "msx"
+    addSystem "zxspectrum"
 }
