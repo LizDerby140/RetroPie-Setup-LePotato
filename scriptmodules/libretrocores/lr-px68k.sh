b3d6bd1866d1f948d152d77b8624f873f7b553bf
LizDerby140 <64528822+LizDerby140@users.noreply.github.com>
2025-03-06 17:43:54 -0700

    Update lr-px68k.sh

--- scriptmodules/libretrocores/lr-px68k.sh
+++ scriptmodules/libretrocores/lr-px68k.sh
@@ -14,7 +14,7 @@ rp_module_desc="SHARP X68000 Emulator"
 rp_module_help="You need to copy a X68000 bios file (iplrom30.dat, iplromco.dat, iplrom.dat, or iplromxv.dat), and the font file (cgrom.dat or cgrom.tmp) to $biosdir/keropi. Use F12 to access the in emulator menu."
 rp_module_repo="git https://github.com/libretro/px68k-libretro.git master"
 rp_module_section="exp"
-rp_module_flags="lepotato 64bit aarch64 arm mali"
+rp_module_flags=""
 
 function sources_lr-px68k() {
     gitPullOrClone
