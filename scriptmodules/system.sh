3c138e68d49ef45238062cf64dd0eb2c001ca1d3
LizDerby140 <64528822+LizDerby140@users.noreply.github.com>
2025-03-06 21:33:06 -0700

    Update system.sh

--- scriptmodules/system.sh
+++ scriptmodules/system.sh
@@ -559,7 +559,7 @@ function cpu_armv8() {
         __default_cpu_flags+="  -mfpu=neon-fp-armv8"
         __platform_flags+=(arm armv8 neon)
     else
-        __platform_flags+=(armv8)
+        __platform_flags+=(arm64)
     fi
     __qemu_cpu="$cpu"
 }
@@ -707,6 +707,6 @@ function platform_vero4k() {
 
 function platform_lepotato() {
     cpu_armv8 "cortex-a53"
-    __platform_flags+=(arm mali gles kms drm)
+    __platform_flags+=(arm armv8 neon mesa gles)
     __default_cflags="-O2 -march=armv8-a+crc+crypto -mtune=cortex-a53"
 }
