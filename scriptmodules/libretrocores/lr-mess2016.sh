e6c558dbdde7301fabe5c03c2f4eacc056382292
LizDerby140 <64528822+LizDerby140@users.noreply.github.com>
2025-03-06 17:39:10 -0700

    Update lr-mess2016.sh

--- scriptmodules/libretrocores/lr-mess2016.sh
+++ scriptmodules/libretrocores/lr-mess2016.sh
@@ -15,7 +15,7 @@ rp_module_help="see wiki for detailed explanation"
 rp_module_licence="GPL2 https://raw.githubusercontent.com/libretro/mame2016-libretro/master/LICENSE.md"
 rp_module_repo="git https://github.com/libretro/mame2016-libretro.git master"
 rp_module_section="exp"
-rp_module_flags="lepotato 64bit aarch64 arm mali"
+rp_module_flags=""
 
 function depends_lr-mess2016() {
     depends_lr-mame2016
