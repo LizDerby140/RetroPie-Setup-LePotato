a7e18c022258daab8f130d2f4885b7c9f98dba9b
LizDerby140 <64528822+LizDerby140@users.noreply.github.com>
2025-03-07 02:07:16 -0700

    Update kodi.sh

--- scriptmodules/ports/kodi.sh
+++ scriptmodules/ports/kodi.sh
@@ -1,62 +1,86 @@
 #!/usr/bin/env bash
 
-# This file is part of The RetroPie Project
+# This file is part of the RetroPie Project
 #
-# The RetroPie Project is the legal property of its developers, whose names are
-# too numerous to list here. Please refer to the COPYRIGHT.md file distributed with this source.
-#
-# See the LICENSE.md file at the top-level directory of this distribution and
-# at https://raw.githubusercontent.com/RetroPie/RetroPie-Setup/master/LICENSE.md
+# The RetroPie Project is the legal property of its developers.
+# Please refer to the LICENSE file distributed with this source.
 #
 
-rp_module_id="kodi"
-rp_module_desc="Kodi - Open source home theatre software"
-rp_module_licence="GPL2 https://raw.githubusercontent.com/xbmc/xbmc/master/LICENSE.GPL"
+rp_module_id="kodi_lepotato"
+rp_module_desc="Kodi - Open source home theatre software optimized for Le Potato (AML-S905X-CC)"
+rp_module_licence="GPL2 https://raw.githubusercontent.com/xbmc/xbmc/master/LICENSE.md"
 rp_module_section="opt"
-rp_module_flags=""
+rp_module_flags="!x86 !mali !rpi"
 
-function _update_hook_kodi() {
-    # to show as installed in retropie-setup 4.x
-    hasPackage kodi && mkdir -p "$md_inst"
-}
-
-function depends_kodi() {
-    if isPlatform "rpi"; then
-        if [[ "$md_mode" == "install" ]]; then
-            # remove old repository
-            rm -f /etc/apt/sources.list.d/mene.list
-            echo "deb http://pipplware.pplware.pt/pipplware/dists/$__os_codename/main/binary/ ./" >/etc/apt/sources.list.d/pipplware.list
-            wget -q -O- http://pipplware.pplware.pt/pipplware/key.asc | apt-key add - &>/dev/null
-        else
-            rm -f /etc/apt/sources.list.d/pipplware.list
-            apt-key del 4096R/BAA567BB >/dev/null
-        fi
-    elif isPlatform "x86" && [[ "$md_mode" == "install" ]]; then
-        apt-add-repository -y ppa:team-xbmc/ppa
+function depends_kodi_lepotato() {
+    # Ensure we're running on Le Potato
+    if ! grep -q "AML-S905X-CC\|Le Potato" /proc/device-tree/model 2>/dev/null; then
+        md_ret_errors+=("This script is optimized for Le Potato (AML-S905X-CC) and may not work on other devices.")
+        return 1
     fi
 
-    # required for reboot/shutdown options. Don't try and remove if removing dependencies
-    [[ "$md_mode" == "install" ]] && getDepends policykit-1
+    getDepends git curl wget unzip lsb-release apt-transport-https software-properties-common \
+               kodi kodi-peripheral-joystick kodi-vfs-libarchive kodi-inputstream-adaptive
 
     addUdevInputRules
 }
 
-function install_bin_kodi() {
-    # force aptInstall to get a fresh list before installing
+function install_bin_kodi_lepotato() {
+    # Ensure apt update is run
     __apt_update=0
-    aptInstall kodi kodi-peripheral-joystick kodi-inputstream-adaptive kodi-inputstream-rtmp kodi-vfs-libarchive kodi-vfs-sftp kodi-vfs-nfs
+
+    local all_pkgs=(kodi kodi-peripheral-joystick kodi-inputstream-adaptive kodi-vfs-libarchive)
+    local avail_pkgs=()
+    
+    for pkg in "${all_pkgs[@]}"; do
+        local ret=$(apt-cache madison "$pkg" 2>/dev/null)
+        [[ -n "$ret" ]] && avail_pkgs+=("$pkg")
+    done
+
+    aptInstall "${avail_pkgs[@]}"
 }
 
-function remove_kodi() {
-    aptRemove kodi
-    rp_callModule kodi depends remove
-}
-
-function configure_kodi() {
-    # remove old directLaunch entry
-    delSystem "$md_id" "kodi"
-
+function configure_kodi_lepotato() {
     moveConfigDir "$home/.kodi" "$md_conf_root/kodi"
 
-    addPort "$md_id" "kodi" "Kodi" "kodi-standalone"
+    # Create and enable systemd service for Kodi
+    cat > /etc/systemd/system/kodi.service << _EOF_
+[Unit]
+Description=Kodi Media Center
+After=systemd-user-sessions.service network-online.target sound.target
+Requires=network-online.target
+
+[Service]
+User=kodi
+Group=kodi
+Type=simple
+ExecStart=/usr/bin/kodi-standalone
+Restart=on-abort
+RestartSec=5
+
+[Install]
+WantedBy=multi-user.target
+_EOF_
+
+    systemctl enable kodi.service
+
+    # Optimize system for Kodi and RetroPie
+    echo "vm.swappiness=10" > /etc/sysctl.d/99-swappiness.conf
+    sysctl -p /etc/sysctl.d/99-swappiness.conf
+
+    # Create RetroPie integration
+    mkdir -p "$home/RetroPie/roms/ports"
+    cat > "$home/RetroPie/roms/ports/kodi.sh" << _EOF_
+#!/bin/bash
+systemctl start kodi.service
+_EOF_
+    chmod +x "$home/RetroPie/roms/ports/kodi.sh"
+
+    addPort "$md_id" "kodi" "Kodi Media Center" "kodi-standalone"
+}
+
+function remove_kodi_lepotato() {
+    aptRemove kodi
+    systemctl disable kodi.service
+    rm -f /etc/systemd/system/kodi.service
 }
