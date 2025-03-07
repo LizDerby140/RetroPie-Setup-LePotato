ae6869b4f12abdb3f894e3d6a54d3252a70ebf75
LizDerby140 <64528822+LizDerby140@users.noreply.github.com>
2025-03-06 17:08:40 -0700

    Update jzintv.sh

--- scriptmodules/emulators/jzintv.sh
+++ scriptmodules/emulators/jzintv.sh
@@ -15,7 +15,7 @@ rp_module_help="ROM Extensions: .int .bin .rom\n\nCopy your Intellivision roms t
 rp_module_licence="GPL2 http://spatula-city.org/%7Eim14u2c/intv/"
 rp_module_repo="file $__archive_url/jzintv-20200712-src.zip"
 rp_module_section="opt"
-rp_module_flags="sdl2 nodistcc lepotato 64bit aarch64 arm mali"
+rp_module_flags=""
 
 function depends_jzintv() {
     getDepends libsdl2-dev libreadline-dev
